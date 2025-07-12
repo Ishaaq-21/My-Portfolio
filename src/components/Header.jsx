9ced6803368a86919be08aea4cc69f713f6bac84
isaac <ishak.hadj14@gmail.com>
2025-07-12 13:46:34 +0100

    refactor: refactor the component and move the container down inside the header compo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,37 +16,35 @@ export default function Header() {
   }
 
   return (
-    <header className="flex justify-between items-center md:h-[70px]">
-      <p className="font-bold hidden md:block text-2xl text-primary hover:text-primary-dark">
-        <a href="/">Isaac</a>
-      </p>
-      <nav className="hidden md:block">
-        <ul className="flex justify-around gap-8 text-sm lg:text-lg">
-          {navItems.map((link) => {
-            return (
-              <li key={link.href}>
-                <a className="font-medium text-base" href={link.href}>
-                  {link.label}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-      <div
-        className={clsx(
-          "md:hidden w-full absolute flex justify-between items-center bottom-0 left-0 h-[45px] px-6  text-primary z-10",
-          isOpen ? "bg-zinc-100" : "rounded-t-lg shadow-top"
-        )}
-      >
-        <span className={clsx("font-semibold text-xl ", isOpen && "hidden")}>
-          Isaac
-        </span>
+    <header className="shadow-top  md:shadow-none left-0 bottom-0 md:top-0  bg-secondary-bg fixed w-full">
+      <div className="container mx-auto px-4 md:px-8 flex justify-between items-center h-[45px] md:h-[70px]">
+        <p
+          className={clsx(
+            "font-semibold  text-lg md:text-2xl text-primary hover:text-primary-dark transition-colors duration-300",
+            isOpen && "hidden"
+          )}
+        >
+          <a href="/">Isaac</a>
+        </p>
+        <nav className="hidden md:block">
+          <ul className="flex justify-around gap-8 text-sm lg:text-lg">
+            {navItems.map((link) => {
+              return (
+                <li key={link.href}>
+                  <a className="font-medium text-base" href={link.href}>
+                    {link.label}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
         <button
           aria-label="Toggle mobile menu"
           aria-expanded={isOpen}
           className={clsx(
-            " hover:text-primary-dark transition-color duration-300 cursor-pointer ml-auto font-bold"
+            "md:hidden hover:text-primary-dark transition-colors duration-300 cursor-pointer ml-auto font-bold"
           )}
           onClick={handleToggleMenu}
         >
@@ -58,33 +56,33 @@ export default function Header() {
             )}
           ></i>
         </button>
-      </div>
-      <nav
-        className={clsx(
-          "absolute md:hidden w-full pt-8 shadow-top left-0 transition-bottom bg-zinc-100 transition-all duration-300",
+        <nav
+          className={clsx(
+            "absolute md:hidden w-full pt-6 pb-3 shadow-top left-0 transition-bottom bg-primary-bg transition-all duration-300 rounded-t-lg",
 
-          isOpen ? "bottom-[45px]" : "-bottom-full"
-        )}
-      >
-        <ul className="grid grid-cols-3  gap-y-5 place-items-center">
-          {navItems.map((link) => {
-            return (
-              <li>
-                <a
-                  key={link.href}
-                  href={link.href}
-                  className="w-full text-center font-medium text-sm sm:text-lg"
-                >
-                  <i
-                    className={`uis ${link.icon} block text-lg sm:text-2xl mr-1 mb-1`}
-                  ></i>
-                  {link.label}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+            isOpen ? "bottom-[45px]" : "-bottom-52"
+          )}
+        >
+          <ul className="grid grid-cols-3  gap-y-5 place-items-center">
+            {navItems.map((link) => {
+              return (
+                <li>
+                  <a
+                    key={link.href}
+                    href={link.href}
+                    className="w-full text-center font-medium text-sm sm:text-lg"
+                  >
+                    <i
+                      className={`uis ${link.icon} block text-lg sm:text-2xl mr-1 mb-1`}
+                    ></i>
+                    {link.label}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
