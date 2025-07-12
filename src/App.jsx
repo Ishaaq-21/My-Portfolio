738789a98f52413e1e1b72b23549cc8b50e1d121
isaac <ishak.hadj14@gmail.com>
2025-07-12 11:16:31 +0100

    set a container that wraps all the app components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
-
 import "./App.css";
-
+import Header from "./components/Header";
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container mx-auto px-8   h-full ">
+        <Header></Header>
+      </div>
+    </>
+  );
 }
 
 export default App;
