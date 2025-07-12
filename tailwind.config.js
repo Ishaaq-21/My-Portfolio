e899d22f2629919f0d567487ceee67b24079db45
isaac <ishak.hadj14@gmail.com>
2025-07-12 08:03:09 +0100

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    container: {
+      center: true,
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: colors.amber[400],
+        "primary-dark": colors.amber[500],
+      },
+    },
+  },
+  plugins: [],
+};
