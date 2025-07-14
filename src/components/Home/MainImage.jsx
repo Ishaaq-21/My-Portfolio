afd55d50735b249655ade9e434ef77c7b89bd3c2
isaac <ishak.hadj14@gmail.com>
2025-07-14 12:54:50 +0100

    style: add border animation to the image

--- src/components/Home/MainImage.jsx
+++ src/components/Home/MainImage.jsx
@@ -1,6 +1,10 @@
 export default function MainImg() {
   return (
-    <div className="image rounded-full  ring-8 ring-primary w-48 max-w-48 h-48 max-h-48 lg:w-80 lg:max-w-80 md:w-60 md:max-w-60 md:h-60 md:max-h-60 lg:h-80 lg:max-h-80   overflow-hidden order-1 md:order-2 mx-auto -translate-x-5 my-5 md:my-5 mx-auto lg:mx-0 lg:grow-[1]">
+    <div
+      className="profile-pic  w-48 max-w-48 h-48 max-h-48 md:w-60 md:max-w-60 md:h-60 md:max-h-60 lg:w-96 lg:max-w-80  lg:h-80 lg:max-h-80
+      rounded-full  ring-8 ring-primary/70 overflow-hidden order-1 md:order-2
+      mx-auto -translate-x-5 my-5 md:my-5 mx-auto lg:mx-0 lg:grow-[1]"
+    >
       <img
         className="w-full h-full "
         src="/public/assets/MyPic.png"
