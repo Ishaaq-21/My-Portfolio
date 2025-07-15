639e85cf6a94c548331be76d7b3d2b2bcc29d328
isaac <ishak.hadj14@gmail.com>
2025-07-15 07:42:59 +0100

    fix: fix the h in large screens

--- src/components/Home/MainImage.jsx
+++ src/components/Home/MainImage.jsx
@@ -1,7 +1,7 @@
 export default function MainImg() {
   return (
     <div
-      className="profile-pic  w-48 max-w-48 h-48 max-h-48 md:w-60 md:max-w-60 md:h-60 md:max-h-60 lg:w-96 lg:max-w-80  lg:h-80 lg:max-h-80
+      className="profile-pic  w-48 max-w-48 h-48 max-h-48 md:w-60 md:max-w-60 md:h-60 md:max-h-60 lg:w-80 lg:max-w-80  lg:h-80 lg:max-h-80
       rounded-full  ring-8 ring-primary/70 overflow-hidden order-1 md:order-2
       mx-auto -translate-x-5 my-5 md:my-5 mx-auto lg:mx-0 lg:grow-[1]"
     >
