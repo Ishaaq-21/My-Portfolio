ed5574b1075ff427e5af22029cf14d816edc10b5
isaac <ishak.hadj14@gmail.com>
2025-07-15 07:39:52 +0100

    style: edit the main wrapper style | add the About section compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Home from "./components/Home/Home";
+import About from "./components/About";
 function App() {
   return (
     <>
       <Header></Header>
-      <main className="w-full h-full">
+      <main className="w-full h-full bg-primary-bg md:pt-[70px]">
         {" "}
         <Home></Home>
+        <About />
       </main>
     </>
   );
