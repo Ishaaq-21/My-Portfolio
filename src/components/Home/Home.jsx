a90cb7dd93e62bd4c68cea096864ea0753710714
isaac <ishak.hadj14@gmail.com>
2025-07-15 09:56:19 +0100

    style: decrease vertical padding of sections in mobile screens

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import SocialLinks from "./SocialLinks";
 
 export default function Home() {
   return (
-    <section className="w-full pt-[45px] md:pt-[70px] h-[calc(100%-45px)] md:h-[calc(100%-70px)] py-20 ">
+    <section className="w-full pt-[45px] md:pt-[70px] h-[calc(100%-45px)] md:h-[calc(100%-70px)] py-12 md:py-20 ">
       <div className="container mx-auto px-6 md:px-8 w-full h-full flex items-center flex-wrap md:flex-nowrap gap-x-2 gap-y-2 md:gap-x-6">
         <SocialLinks />
         <MainContent />
