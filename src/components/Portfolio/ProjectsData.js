48d8062b3574dee8d03b390610dd6c5331f28450
isaac <ishak.hadj14@gmail.com>
2025-07-19 09:52:33 +0100

    refactor: move the projectsdata arr to a seperate file

---
+++ src/components/Portfolio/ProjectsData.js
@@ -0,0 +1,95 @@
+const projectsData = [
+  {
+    icon: "/public/assets/Projects/DVLD.png",
+    type: "desktop",
+    name: "Driving License Management System (DVLD)",
+    description:
+      "A C# desktop app for managing driving license issuance, renewal, and verification with SQL Server support",
+    demo: "https://vimeo.com/1056301250/53c69bb538",
+    github: "https://github.com/Ishaaq-21/Driving-Management-System-",
+  },
+  {
+    icon: "/public/assets/Projects/PrayerTimes.png",
+    type: "react",
+    name: "Prayer Times for muslims",
+    description:
+      "Responsive app showing global prayer times, live clock, countdown, multilingual support, and AI Islamic assistant.",
+    demo: "https://prayer-times-for-muslims.netlify.app/",
+    github: "https://github.com/Ishaaq-21/Prayer-Times-React-App",
+  },
+  {
+    icon: "/public/assets/Projects/Portfolio.png",
+    type: "react",
+    name: "My Portfolio",
+    description:
+      "A sleek, responsive web portfolio to showcase my projects, skills, and background, built with modern web technologies.",
+    github: "https://github.com/Ishaaq-21/My-Portfolio",
+  },
+
+  {
+    icon: "/public/assets/Projects/creative-mini.png",
+    type: "Html-Css-Js",
+    name: "Creative Agency Portfolio",
+    description:
+      "A modern, responsive website for a creative agency with interactive sections, theme customization — built using HTML, CSS, and JavaScript.",
+    demo: "https://ishaaq-21.github.io/Creative-Agency/",
+    github: "https://github.com/Ishaaq-21/Creative-Agency",
+  },
+  {
+    icon: "/public/assets/Projects/dash.png",
+    type: "Html-Css",
+    name: "Admin Dashboard Template",
+    description:
+      "A modern admin dashboard with task, file, and project management features. Built for clarity, responsiveness, and smooth user workflows.",
+    demo: "https://ishaaq-21.github.io/Admin-Dashboard/",
+    github: "https://github.com/Ishaaq-21/Admin-Dashboard",
+  },
+  {
+    icon: "/public/assets/Projects/master-mini.png",
+
+    type: "Html-Css",
+    name: "Master Template",
+    description:
+      "A clean, multipurpose business template with sections for articles, features, and testimonials. Ideal for scalable, professional websites.",
+    demo: "https://ishaaq-21.github.io/Html-Css-Template-master/",
+    github: "https://github.com/Ishaaq-21/Html-Css-Template-master",
+  },
+  {
+    icon: "/public/assets/Projects/todo.png",
+    type: "react",
+    name: "Todo App",
+    description:
+      "Modern, responsive todo app with full CRUD, filtering, local storage, and context-driven state using React, Material-UI, and Vite.",
+    demo: "https://ishaaq-21.github.io/React-TodoApp/",
+    github: "https://github.com/Ishaaq-21/React-TodoApp",
+  },
+  {
+    icon: "/public/assets/Projects/leon.png",
+    type: "Html-Css",
+    name: "Leon Template",
+    description:
+      "A sleek landing template with services, portfolio, and pricing sections. Built using Flexbox, Grid, and polished animations.",
+    demo: "https://ishaaq-21.github.io/Html-Css-Leon-template/",
+    github: "https://github.com/Ishaaq-21/Html-Css-Leon-template",
+  },
+  {
+    icon: "/public/assets/Projects/kasper-mini.png",
+    type: "Html-Css",
+    name: "Kasper Template",
+    description:
+      "A minimalist portfolio template with animated sections and a responsive layout. Great for freelancers or creative agencies.",
+    demo: "https://ishaaq-21.github.io/Html-Css-Kasper-Template/",
+    github: "https://github.com/Ishaaq-21/Html-Css-Kasper-Template",
+  },
+  {
+    icon: "/public/assets/Projects/miniJs.png",
+    type: "Html-Css-Js",
+    name: "Mini-Projects",
+    description:
+      "A web app with four interactive sections: custom cursor effects, typing animation, live calendar, and city-based weather forecast.",
+    demo: "https://ishaaq-21.github.io/Vanilla-Js-Mini-Projects/",
+    github: "https://github.com/Ishaaq-21/Vanilla-Js-Mini-Projects",
+  },
+];
+
+export default projectsData;
