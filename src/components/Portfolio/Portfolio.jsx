c0b6b220ec2672584c8642f47f4afe913481e551
isaac <ishak.hadj14@gmail.com>
2025-07-19 09:54:55 +0100

    move the compo to a new folder

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import SectionHeader from "../SharedComponents/SectionHeader";
+import projectsData from "./ProjectsData";
+import ProjectCard from "./ProjectCard";
+
+export default function Portfolio() {
+  //I will implement this later, ignore it
+  const [filter, setFilter] = useState([]);
+  const [showMore, setShowMore] = useState(false);
+  const finalProjectsArr = showMore ? projectsData : projectsData.slice(0, 6);
+  return (
+    <section className="Portfolio px-7 md:px-12 py-12 lg:py-16 relative  ">
+      <SectionHeader title={"Portfolio"} subTitle={"My Work"} />
+
+      <ul className="grid grid-cols-[repeat(1,_minmax(0,_400px))]   sm:grid-cols-2 xl:grid-cols-[repeat(3,_minmax(0,_450px))] gap-5 justify-center">
+        {finalProjectsArr.map((projectItem) => {
+          return <ProjectCard project={projectItem} />;
+        })}
+      </ul>
+      <button
+        className="main-btn !block !mx-auto mt-8"
+        onClick={() => setShowMore((prev) => !prev)}
+      >
+        {showMore ? "Show Less" : "Show More"}
+      </button>
+    </section>
+  );
+}
