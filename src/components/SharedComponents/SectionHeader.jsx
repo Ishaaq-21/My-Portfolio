d5da7fefc0fe9f04395ef5baa6cda4bfb4c9dce0
isaac <ishak.hadj14@gmail.com>
2025-07-19 12:51:56 +0100

    style: decrease the font-size of the section title

--- src/components/SharedComponents/SectionHeader.jsx
+++ src/components/SharedComponents/SectionHeader.jsx
@@ -1,7 +1,7 @@
 export default function SectionHeader({ title, subTitle }) {
   return (
     <div className="mb-14">
-      <h1 className="text-title font-bold w-fit  mx-auto text-3xl lg:text-5xl">
+      <h1 className="text-title font-bold w-fit  mx-auto text-3xl md:text-4xl">
         {title}
       </h1>
       <h4 className="text-subTitle text-sm w-fit mx-auto mt-1">{subTitle}</h4>
