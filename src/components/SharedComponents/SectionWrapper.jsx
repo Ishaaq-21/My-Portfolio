69c85fb74c4d1cbb6d41441d668a1d54c7781362
isaac <ishak.hadj14@gmail.com>
2025-07-19 12:50:23 +0100

    style: remove the min-h class from sectionWrapper

--- src/components/SharedComponents/SectionWrapper.jsx
+++ src/components/SharedComponents/SectionWrapper.jsx
@@ -2,7 +2,7 @@ export default function SectionWrapper({ children, sectionName }) {
   return (
     <section
       id={`#${sectionName}`}
-      className={`${sectionName} py-12 lg:py-20 min-h-[100vh]`}
+      className={`${sectionName} py-12 lg:py-20 `}
     >
       <div className="container px-6 md:px-8 h-full">{children}</div>{" "}
     </section>
