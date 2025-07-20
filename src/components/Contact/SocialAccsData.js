cd78b3d01e22aa68ed1940b7389688f90f8035eb
isaac <ishak.hadj14@gmail.com>
2025-07-20 10:54:09 +0100

    seperate the social accs data to another file

---
+++ src/components/Contact/SocialAccsData.js
@@ -0,0 +1,21 @@
+const socialAccounts = [
+  {
+    icon: "uil uil-envelope-add",
+    name: "Email",
+    data: "ishak.hadj14@gmail.com",
+  },
+  {
+    icon: "uil uil-linkedin",
+    name: "LinkedIn",
+    data: "H. Ishaq",
+    url: "https://www.linkedin.com/in/h-ishaq-hk/",
+  },
+
+  {
+    icon: "uil uil-facebook-messenger",
+    name: "Messenger",
+    data: "hk.ishaq",
+    url: "https://www.messenger.com/e2ee/t/10027776657286032/",
+  },
+];
+export default socialAccounts;
