cb706253ad16098ddb4f7307a656442aa12c68c5
isaac <ishak.hadj14@gmail.com>
2025-07-20 12:22:42 +0100

    refactore: move social links array to a seperate file, then use it through importing

--- src/components/Home/SocialLinks.jsx
+++ src/components/Home/SocialLinks.jsx
@@ -1,23 +1,4 @@
-const socialLinks = [
-  {
-    name: "LinkedIn",
-    icon: "uil-linkedin",
-    href: "https://www.linkedin.com/in/h-ishaq-hk/",
-    hoverColor: "hover:text-[#0A66C2]",
-  },
-  {
-    name: "GitHub",
-    icon: "uil-github",
-    href: "https://github.com/Ishaaq-21",
-    hoverColor: "hover:text-slate-500",
-  },
-  {
-    name: "Facebook",
-    icon: "uil-facebook-f",
-    href: "https://www.facebook.com/hk.ishaq/?locale=ar_AR",
-    hoverColor: "hover:text-[#1877F2]",
-  },
-];
+import socialLinks from "./socialLinks";
 export default function SocialLinks() {
   return (
     <ul className="social-links flex gap-y-3 flex-col    order-0 w-[50px]  lg:w-[118px]">
