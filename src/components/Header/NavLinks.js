c67466fab3c980852068ebfc83cca327977432ec
isaac <ishak.hadj14@gmail.com>
2025-07-21 11:20:14 +0100

    move the array of links data to a seperate file in order to use it through importing

---
+++ src/components/Header/NavLinks.js
@@ -0,0 +1,9 @@
+const navItems = [
+  { label: "Home", icon: "uil-estate", href: "#Home" },
+  { label: "About", icon: "uil-user", href: "#About" },
+  { label: "Skills", icon: "uil-list-ul", href: "#Skills" },
+  { label: "Portfolio", icon: "uil-image", href: "#Portfolio" },
+  { label: "Contact", icon: "uil-message", href: "#Contact" },
+];
+
+export default navItems;
