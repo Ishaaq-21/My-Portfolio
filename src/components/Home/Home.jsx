ad7530c7643fa89bb6eda6270b08364f914e0ad7
isaac <ishak.hadj14@gmail.com>
2025-07-22 19:13:24 +0100

    add an Id to the home element

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,10 @@ import SocialLinks from "./SocialLinks";
 
 export default function Home() {
   return (
-    <section className="home w-full  h-[calc(100%-45px)] md:h-[calc(100%-70px)] py-12 lg:py-20 ">
+    <section
+      id="Home"
+      className="home w-full  h-[calc(100%-45px)] md:h-[calc(100%-70px)] py-12 lg:py-20 "
+    >
       <div className="container mx-auto px-6 md:px-8 w-full h-full flex items-center flex-wrap md:flex-nowrap gap-x-2 gap-y-2 md:gap-x-6">
         <SocialLinks />
         <MainContent />
