7fb6efece0c2799688d9a4157cac0a96b54fcb5f
isaac <ishak.hadj14@gmail.com>
2025-07-27 15:05:46 +0100

    style: adjust the cards border-color to fit the new main bg color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const cards = [
 
 function AboutCard({ icon, title, text }) {
   return (
-    <li className="card w-[90px]   sm:w-[120px] lg:w-[150px] text-center bg-white dark:bg-slate-900 border-2 dark:border-primary-light  py-3 lg:py-5 px-2 lg:px-4 rounded-2xl">
+    <li className="card w-[90px] sm:w-[120px] lg:w-[150px] text-center bg-white dark:bg-slate-900 border-2 border-[#ccc] dark:border-primary-light  py-3 lg:py-5 px-2 lg:px-4 rounded-2xl">
       <i className={`${icon} text-2xl text-primary mb-2  block`}></i>
       <span className="text-[10px] sm:text-sm lg:text-base font-semibold text-title-dark dark:text-white block">
         {title}
