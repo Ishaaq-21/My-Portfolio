3253d69b93d8248fea712241cf9ce5fed31eee5b
isaac <ishak.hadj14@gmail.com>
2025-07-27 19:20:54 +0100

    refactor: remove targetCursor animation in the small screens cuz it has no effect

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,20 @@ import Header from "./components/Header/Header";
 import TargetCursor from "./blocks/Animated UI compo/TargetCursor/TargetCursor";
 
 import AnimateSectionOnScroll from "./ScrollAnimationContainer";
+import { useEffect, useState } from "react";
 function App() {
+  const [mobileScreen, setMobileScreen] = useState(true);
+  useEffect(() => {
+    if (window.innerWidth <= 768) setMobileScreen(true);
+    else setMobileScreen(false);
+  }, []);
+
   return (
     <>
-      <TargetCursor spinDuration={2} hideDefaultCursor={true} />
+      {/* the target cursor has no effect in mobile screens since they work with touch, so I render the component only in pc's and large screens */}
+      {!mobileScreen && (
+        <TargetCursor spinDuration={2} hideDefaultCursor={true} />
+      )}
       <Header></Header>
       <AnimateSectionOnScroll />
     </>
