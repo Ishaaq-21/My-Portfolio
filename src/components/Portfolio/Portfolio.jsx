434cde0066389193138f2c21395904e09db926e5
isaac <ishak.hadj14@gmail.com>
2025-07-30 19:53:37 +0100

    refactor: change the sectionWrapper structure and logic to fit the Portfolio section wrap the portfolio section in the section wrapper

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,16 +2,14 @@ import { useState } from "react";
 import SectionHeader from "../SharedComponents/SectionHeader";
 import projectsData from "./ProjectsData";
 import ProjectCard from "./ProjectCard";
+import SectionWrapper from "../SharedComponents/SectionWrapper";
 
 export default function Portfolio() {
   //I will implement this later, ignore it
   const [showMore, setShowMore] = useState(false);
   const finalProjectsArr = showMore ? projectsData : projectsData.slice(0, 6);
   return (
-    <section
-      id="Portfolio"
-      className="hidden-section Portfolio px-7 md:px-12 py-12 lg:py-16 relative  "
-    >
+    <SectionWrapper sectionName={"Portfolio"}>
       <SectionHeader title={"Portfolio"} subTitle={"My Work"} />
 
       <ul className="grid grid-cols-[repeat(1,_minmax(0,_300px))]   sm:grid-cols-2 xl:grid-cols-[repeat(3,_minmax(0,_450px))] gap-5 justify-center">
@@ -25,6 +23,6 @@ export default function Portfolio() {
       >
         {showMore ? "Show Less" : "Show More"}
       </button>
-    </section>
+    </SectionWrapper>
   );
 }
--- src/components/SharedComponents/SectionWrapper.jsx
+++ src/components/SharedComponents/SectionWrapper.jsx
@@ -2,9 +2,17 @@ export default function SectionWrapper({ children, sectionName }) {
   return (
     <section
       id={`${sectionName}`}
-      className={`${sectionName} hidden-section  py-12 lg:py-20 `}
+      className={`${sectionName} ${
+        sectionName === "Portfolio"
+          ? "px-7 md:px-12 lg:py-16 relative"
+          : "lg:py-20"
+      } hidden-section py-12`}
     >
-      <div className="container px-6 md:px-8 h-full">{children}</div>{" "}
+      {sectionName === "Portfolio" ? (
+        children
+      ) : (
+        <div className="container px-6 md:px-8 h-full">{children}</div>
+      )}
     </section>
   );
 }
