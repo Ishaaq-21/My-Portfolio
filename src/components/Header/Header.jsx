ea20bb0cae57a7266491b0dd2e40836b924d3ca2
isaac <ishak.hadj14@gmail.com>
2025-07-31 11:18:03 +0100

    style: decrease the opacity of the header bg-color

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
   }
 
   return (
-    <header className="shadow-top  left-0 bottom-0 md:top-0 z-50 bg-secondary-bg dark:bg-slate-900 fixed w-full  h-[45px] md:h-[70px] md:shadow-[0px_-1px_5px_0px_black] dark:md:shadow-[0px_-1px_5px_0px_white]">
+    <header className="shadow-top  left-0 bottom-0 md:top-0 z-50 bg-secondary-bg dark:bg-slate-900/90 fixed w-full  h-[45px] md:h-[70px] md:shadow-[0px_-1px_5px_0px_black] dark:md:shadow-[0px_-1px_5px_0px_white]">
       <div className="container mx-auto px-6 md:px-8 flex justify-between items-center h-full ">
         <p
           className={`font-semibold  text-lg md:text-2xl text-primary hover:text-primary-dark transition-colors duration-300 cursor-target cursor-none p-2" ${
